0f106123ac23f500653a39d3815ae6f7d289cc55
Bradley Schmerl <schmerl@cs.cmu.edu>
2020-08-28 15:11:41 -0400

    Fixed long start up time and improved build process (fixes #52) (#53)
    * Reorgnaization of file space for more incremental building and move chown to Dockerfile and configure rti to not copy examples
    
    * Address review issues

---
+++ ros2/rootfs/rticommon_config.sh
@@ -0,0 +1,10 @@
+# rticommon_config.sh
+# -------------------
+# copy_workspace enables copying the whole rti_workspace folder. Set to false to disable this.
+copy_workspace=false
+ 
+# copy_examples enables copying the examples directory. Set to false to disable this.
+copy_examples=false
+ 
+# workspace_dir configures where the rti_workspace directory is copied over.
+workspace_dir="/rti_workspace"
