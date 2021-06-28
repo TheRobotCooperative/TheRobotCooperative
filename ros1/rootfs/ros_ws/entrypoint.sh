16295abba0a82fd30f9f705c926fd815abe34c8c
ChrisTimperley <christimperley@googlemail.com>
2021-06-28 18:34:59 -0400

    Added workaround to deal with robots that write to devel_isolated

--- ros1/rootfs/ros_ws/entrypoint.sh
+++ ros1/rootfs/ros_ws/entrypoint.sh
@@ -1,5 +1,14 @@
-#!/bin/bash 
-set -e 
-source "/opt/ros/${ROS_DISTRO}/setup.bash" 
-source "/ros_ws/devel/setup.bash" 
+#!/bin/bash
+set -e
+source "/opt/ros/${ROS_DISTRO}/setup.bash"
+
+if [ -f "/ros_ws/devel/setup.bash" ]; then
+  source "/ros_ws/devel/setup.bash"
+elif [ -f "/ros_ws/devel_isolated/setup.bash" ]; then
+  source "/ros_ws/devel_isolated/setup.bash"
+else
+  echo "Failed to find appropriate setup.bash in /ros_ws"
+  exit 1
+fi
+
 exec "$@"
