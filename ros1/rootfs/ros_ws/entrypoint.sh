f73c53d37300a9485bdbf75e8272a9e51c414f10
ChrisTimperley <christimperley@googlemail.com>
2021-06-28 19:27:43 -0400

    fixed bad existence check

--- ros1/rootfs/ros_ws/entrypoint.sh
+++ ros1/rootfs/ros_ws/entrypoint.sh
@@ -2,9 +2,9 @@
 set -e
 source "/opt/ros/${ROS_DISTRO}/setup.bash"
 
-if [ -d "/ros_ws/devel/setup.bash" ]; then
+if [ -f "/ros_ws/devel/setup.bash" ]; then
   source "/ros_ws/devel/setup.bash"
-elif [ -d "/ros_ws/devel_isolated/setup.bash" ]; then
+elif [ -f "/ros_ws/devel_isolated/setup.bash" ]; then
   source "/ros_ws/devel_isolated/setup.bash"
 else
   echo "Failed to find appropriate setup.bash in /ros_ws"
